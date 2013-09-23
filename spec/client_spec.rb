de2f505a0349682ead784a4e3e0ef4402451f5c4
Carsten Zimmermann <carp@hacksocke.de>
2013-09-23 17:49:10 +0200

    Test existence of send_event

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe EventGirl::Client do
+
+  subject { described_class.new('http://example.com', 'foobar') }
+
   it 'creates instances with two arguments' do
     expect(described_class.instance_method(:initialize).arity).to eql 2
   end
@@ -13,5 +16,11 @@ describe EventGirl::Client do
   it 'sets the url' do
     subject = described_class.new('http://localhost:3000', nil)
     expect(subject.url).to eql 'http://localhost:3000'
+  end
+
+  describe '#send_event' do
+    it 'requires the event title as argument' do
+      expect(subject.method(:send_event).arity).to eql 1
+    end
   end
 end
